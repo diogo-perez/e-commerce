2876117742a294e5d3919aa900e6d55a1f677e37
Diogo Perez <perezdiogo1@gmail.com>
2024-07-22 17:50:04 -0400

    fix:correção de bug no template

--- src/components/template/Pagina.tsx
+++ src/components/template/Pagina.tsx
@@ -9,7 +9,7 @@ export interface PaginaProps {
 export default function Pagina(props: PaginaProps) {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar pesquisaProduto={props.pesquisaProduto} />
+      <Navbar />
       <main
         className={`w-full max-w-[1200px] mx-auto flex-1 ${
           props.className ?? ""
