3153e40bc5ca3745afd6f6393277fd4d83014502
Diogo Perez <perezdiogo1@gmail.com>
2024-07-22 18:05:54 -0400

    style: icone de limpar input de pesquisa

--- src/components/produto/PesquisaProduto.tsx
+++ src/components/produto/PesquisaProduto.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { IconSearch } from "@tabler/icons-react";
+import { IconSearch, IconX } from "@tabler/icons-react";
 import Produto from "@/data/model/Produto";
 
 export interface PesquisaProdutoProps {
@@ -32,20 +32,33 @@ export default function PesquisaProduto(props: PesquisaProdutoProps) {
     setProdutosFiltrados([]);
   };
 
+  const handleClear = () => {
+    setTexto("");
+    setProdutosFiltrados([]);
+    props.onProdutosFiltrados?.([]);
+  };
+
   return (
-    <div className="absolute w-4/5 sm:w-3/5 left-1/2 transform -translate-x-1/2 z-20 flex">
+    <div className="relative w-4/5 sm:w-3/5 left-1/2 transform -translate-x-1/2 z-20 flex">
       <div className="relative w-full">
         <input
           type="text"
           value={texto}
           onChange={handleChange}
-          className="w-full p-3 pl-10 bg-zinc-800 text-white placeholder-white focus:outline-none border-2 border-white rounded-full"
+          className="w-full p-3 pl-10 pr-12 bg-zinc-800 text-white placeholder-white focus:outline-none border-2 border-white rounded-full"
           placeholder="Pesquisar produtos..."
         />
         <IconSearch
           className="absolute left-3 top-1/2 transform -translate-y-1/2 text-white"
           size={20}
         />
+        {texto && (
+          <IconX
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white cursor-pointer"
+            size={20}
+            onClick={handleClear}
+          />
+        )}
         {produtosFiltrados.length > 0 && (
           <ul className="absolute left-0 right-0 bg-zinc-800 mt-1 max-h-60 overflow-y-auto rounded-md shadow-lg z-10">
             {produtosFiltrados.map((produto) => (
